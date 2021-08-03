a441149aa3360d4d2f0456e23804c633bfd1d684
Jack Meng <error666jm@gmail.com>
2021-08-02 22:30:53 -0400

    Create Form2.cpp

---
+++ c++/Form2.cpp
@@ -0,0 +1,8 @@
+//source: http://www.cplusplus.com/forum/lounge/79437/
+
+#include <iostream>
+
+int main()
+{
+    return static_cast<int> (static_cast<bool> (std::endl(operator<<(std::cout, "Hello, World!"))));
+}
