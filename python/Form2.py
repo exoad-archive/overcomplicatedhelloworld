e954479668466fe849e89875d7d5c3d86c4efec7
Jack Meng <error666jm@gmail.com>
2021-08-03 08:45:08 -0400

    Create Form2.py

---
+++ python/Form2.py
@@ -0,0 +1,22 @@
+# Unable to disclose source
+
+import random
+shit = "Hello world!"
+def please(killme):
+  ihatemylife = random.randint(0,len(killme)-1)
+  return killme[ihatemylife]
+def balls(dicks):
+  asshole = ""
+  while len(asshole) != len(dicks):
+    asshole += please(dicks)
+  return asshole
+def fuck(shit, bananas):
+  damn = 0
+  while shit != bananas:
+    shit = balls(bananas)
+    damn += 1
+  return (shit,damn)
+print("starting...")
+holy = fuck(balls(shit),shit)
+print(holy[0] + "\r\nOnly took " + holy[1] + "tries!")
+input()
